d71d84de7c2cea4f1bbe812488f686c5e5076065
EmreBHCC <emrebahceci38@gmail.com>
2024-11-05 20:36:05 +0300

    page-scrapper

--- src/main.rs
+++ src/main.rs
@@ -1,9 +1,8 @@
 use scraper;
 use reqwest;
 use csv;
+use std::error::Error;
 
-// define a custom data structure
-// to store the scraped data
 struct Product {
     url: Option<String>,
     image: Option<String>,
@@ -11,76 +10,79 @@ struct Product {
     price: Option<String>,
 }
 
-fn main() {
-    // initialize the vector that will store the scraped data
+fn main() -> Result<(), Box<dyn Error>> {
     let mut products: Vec<Product> = Vec::new();
-    // download the target HTML document
-    let response = reqwest::blocking::get("https://www.scrapingcourse.com/ecommerce/");
-    // get the HTML content from the request response
-    let html_content = response.unwrap().text().unwrap();
-    // parse the HTML document
-    let document = scraper::Html::parse_document(&html_content);
 
-    // define the CSS selector to get all product
-    // on the page
-    let html_product_selector = scraper::Selector::parse("li.product").unwrap();
-    // apply the CSS selector to get all products
-    let html_products = document.select(&html_product_selector);
+    for page_number in 1..=12 {
+        // Construct the URL for each page
+        let url = format!("https://www.scrapingcourse.com/ecommerce/page/{}/", page_number);
 
-    // iterate over each HTML product to extract data
-    // from it
-    for html_product in html_products {
-        // scraping logic to retrieve the info
-        // of interest
-        let url = html_product
-            .select(&scraper::Selector::parse("a").unwrap())
-            .next()
-            .and_then(|a| a.value().attr("href"))
-            .map(str::to_owned);
-        let image = html_product
-            .select(&scraper::Selector::parse("img").unwrap())
-            .next()
-            .and_then(|img| img.value().attr("src"))
-            .map(str::to_owned);
-        let name = html_product
-            .select(&scraper::Selector::parse("h2").unwrap())
-            .next()
-            .map(|h2| h2.text().collect::<String>());
-        let price = html_product
-            .select(&scraper::Selector::parse(".price").unwrap())
-            .next()
-            .map(|price| price.text().collect::<String>());
+        // Download the target HTML document
+        let response = reqwest::blocking::get(&url)?;
 
-        // instanciate a new product
-        // with the scraped data and add it to the list
-        let product = Product {
-            url,
-            image,
-            name,
-            price,
-        };
+        // If the page doesn't exist or cannot be accessed, skip to the next page
+        if response.status().is_client_error() || response.status().is_server_error() {
+            eprintln!("Page {} could not be accessed.", page_number);
+            continue;
+        }
 
-        products.push(product);
+        // Get the HTML content from the request response
+        let html_content = response.text()?;
+        let document = scraper::Html::parse_document(&html_content);
+
+        // Define the CSS selector to get all products on the page
+        let html_product_selector = scraper::Selector::parse("li.product").unwrap();
+        let html_products = document.select(&html_product_selector);
+
+        // Iterate over each HTML product to extract data
+        for html_product in html_products {
+            let url = html_product
+                .select(&scraper::Selector::parse("a").unwrap())
+                .next()
+                .and_then(|a| a.value().attr("href"))
+                .map(str::to_owned);
+            let image = html_product
+                .select(&scraper::Selector::parse("img").unwrap())
+                .next()
+                .and_then(|img| img.value().attr("src"))
+                .map(str::to_owned);
+            let name = html_product
+                .select(&scraper::Selector::parse("h2").unwrap())
+                .next()
+                .map(|h2| h2.text().collect::<String>());
+            let price = html_product
+                .select(&scraper::Selector::parse(".price").unwrap())
+                .next()
+                .map(|price| price.text().collect::<String>());
+
+            let product = Product {
+                url,
+                image,
+                name,
+                price,
+            };
+
+            products.push(product);
+        }
     }
-    // create the CSV output file
+
+    // Write data to CSV
     let path = std::path::Path::new("products.csv");
-    let mut writer = csv::Writer::from_path(path).unwrap();
+    let mut writer = csv::Writer::from_path(path)?;
 
-    // append the header to the CSV
-    writer
-        .write_record(&["url", "image", "name", "price"])
-        .unwrap();
+    // Append the header to the CSV
+    writer.write_record(&["url", "image", "name", "price"])?;
 
-    // populate the output file
+    // Populate the output file
     for product in products {
-        let url = product.url.unwrap();
-        let image = product.image.unwrap();
-        let name = product.name.unwrap();
-        let price = product.price.unwrap();
-        writer.write_record(&[url, image, name, price]).unwrap();
+        writer.write_record(&[
+            product.url.unwrap_or_default(),
+            product.image.unwrap_or_default(),
+            product.name.unwrap_or_default(),
+            product.price.unwrap_or_default(),
+        ])?;
     }
 
-    // free up the writer resources
-    writer.flush().unwrap();
+    writer.flush()?;
+    Ok(())
 }
-
